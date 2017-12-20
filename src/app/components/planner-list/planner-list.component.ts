bb33b9951c10042a0bf4031eaf8282333b455553
Sanborn <sanborn.sen@gmail.com>
2017-12-20 15:44:32 +0530

    feat(ngrx): Store initial setup is done

--- src/app/components/planner-list/planner-list.component.ts
+++ src/app/components/planner-list/planner-list.component.ts
@@ -163,7 +163,7 @@ export class PlannerListComponent implements OnInit, AfterViewInit, AfterViewChe
     private userService: UserService,
     private urlService: UrlService,
     private renderer: Renderer2,
-    private store: Store<AppState> ) {}
+    private store: Store<AppState>) {}
 
   ngOnInit(): void {
 
