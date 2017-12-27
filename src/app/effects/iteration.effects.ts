59b01a63ff42f7bcbc053190e6480ad39c366bf8
Divyanshi Gupta <divgupta@localhost.localdomain>
2017-12-27 12:56:55 +0530

    feat(iteration-reducer):get-iterations and add-iteration actions now handled in the reducer

--- src/app/effects/iteration.effects.ts
+++ src/app/effects/iteration.effects.ts
@@ -3,7 +3,8 @@ import { Actions, Effect } from "@ngrx/effects";
 import { Observable } from "rxjs";
 import * as IterationActions from ".././actions/iteration.actions";
 import { IterationService } from '.././services/iteration.service';
-import { Action } from '@ngrx/store';
+
+export type Action = IterationActions.All;
 
 @Injectable()
 export class IterationEffects {
@@ -12,10 +13,19 @@ export class IterationEffects {
   }
 
   @Effect() getIterations$ : Observable<Action> = this.actions$
-    .ofType(IterationActions.GET)
+    .ofType<IterationActions.Get>(IterationActions.GET)
     .switchMap(action => {
       return this.iterationService.getIterations()
            .map(iterations => (new IterationActions.GetSuccess(iterations)))
            .catch(() => Observable.of(new IterationActions.GetError()))
+    });
+
+  @Effect() addIteration$ : Observable<Action> = this.actions$
+    .ofType<IterationActions.Add>(IterationActions.ADD)
+    .switchMap(action => {
+      console.log('####-9', action);
+      return this.iterationService.createIteration(action.iteration,action.parentIteration)
+           .map(iterations => (new IterationActions.AddSuccess(iterations)))
+           .catch(() => Observable.of(new IterationActions.AddError()))
     });
 }
