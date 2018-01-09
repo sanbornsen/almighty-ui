e6f3fe03d4338fb4c748653867f8ce61a6c511e4
Nimisha Mukherjee <nimishamukerjee@gmail.com>
2018-01-09 22:36:52 +0530

    fix(side-panel): Include work item type group under mock data.

--- src/app/components/planner-list/planner-list.component.ts
+++ src/app/components/planner-list/planner-list.component.ts
@@ -184,7 +184,7 @@ export class PlannerListComponent implements OnInit, AfterViewChecked, OnDestroy
     // on update the value on URL
 
     const queryParams = this.route.snapshot.queryParams;
-    if(Object.keys(queryParams).length === 0 ) {
+    if(Object.keys(queryParams).length === 0 && process.env.ENV != 'inmemory') {
       this.setDefaultUrl();
     } else {
       if (Object.keys(queryParams).indexOf('iteration') > -1) {
@@ -367,7 +367,7 @@ export class PlannerListComponent implements OnInit, AfterViewChecked, OnDestroy
 
   loadWorkItems(): void {
     const queryParams = this.route.snapshot.queryParams;
-    if(Object.keys(queryParams).length === 0 )
+    if(Object.keys(queryParams).length === 0 && process.env.ENV != 'inmemory')
       this.setDefaultUrl();
     this.uiLockedList = true;
     if (this.wiSubscriber) {
@@ -1096,14 +1096,14 @@ export class PlannerListComponent implements OnInit, AfterViewChecked, OnDestroy
       if (index > -1) {
         // For collapsing
         this.table.rowDetail.toggleExpandRow(this.detailExpandedRows[index]);
-        this.detailExpandedRows.splice(index, 1); 
+        this.detailExpandedRows.splice(index, 1);
       } else {
         // For expanding
         this.detailExpandedRows.forEach(r => {
           this.table.rowDetail.toggleExpandRow(r);
         });
         this.detailExpandedRows = [];
-        this.table.rowDetail.toggleExpandRow(row); 
+        this.table.rowDetail.toggleExpandRow(row);
         this.detailExpandedRows.push(row);
       }
     }
