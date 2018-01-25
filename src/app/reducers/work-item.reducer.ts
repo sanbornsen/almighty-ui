91697a076df76c282352c56604f8b83ce3606af0
Divyanshi Gupta <divgupta@localhost.localdomain>
2018-01-25 13:12:52 +0530

    work in progress

--- src/app/reducers/work-item.reducer.ts
+++ src/app/reducers/work-item.reducer.ts
@@ -3,37 +3,31 @@ import * as WorkItemActions from './../actions/work-item.actions';
 import { WorkItemState, initialState } from './../states/work-item.state';
 import { cloneDeep } from 'lodash';
 
-import { WorkItem } from './../models/work-item';
+import { WorkItemUI } from './../models/work-item';
 
 export type Action = WorkItemActions.All;
 
 export const WorkItemReducer: ActionReducer<WorkItemState> = (state = initialState, action: Action) => {
   switch(action.type) {
     case WorkItemActions.GET_SUCCESS: {
-      return {
-        workItems: cloneDeep(action.payload)
-      }
+      return  cloneDeep(action.payload);
     }
     case WorkItemActions.GET_ERROR: {
       return state;
     }
     case WorkItemActions.ADD_SUCCESS: {
-      return {
-        workItems: [...[action.payload], ...state.workItems]
-      }
+      return state = [...[action.payload], ...state];
     }
     case WorkItemActions.ADD_ERROR: {
       return state;
     }
     case WorkItemActions.UPDATE_SUCCESS: {
       let updatedWorkItem = action.payload;
-      let index = state.workItems.findIndex(wi => wi.id === updatedWorkItem.id);
+      let index = state.findIndex(wi => wi.id === updatedWorkItem.id);
       if (index > -1) {
-        state.workItems[index] = action.payload;
+        state[index] = action.payload;
       }
-      return {
-        workItems: state.workItems
-      }
+      return state;
     }
     case WorkItemActions.UPDATE_ERROR: {
       return state;
