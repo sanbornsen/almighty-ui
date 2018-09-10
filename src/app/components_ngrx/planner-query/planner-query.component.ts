e3c812e24a5248f9ba14057000cc29de38503af9
sanbornsen <sanborn.sen@gmail.com>
2018-09-10 15:38:50 +0530

    WIP

--- src/app/components_ngrx/planner-query/planner-query.component.ts
+++ src/app/components_ngrx/planner-query/planner-query.component.ts
@@ -46,14 +46,6 @@ export class PlannerQueryComponent implements OnInit, OnDestroy, AfterViewChecke
             filters: filters,
             isShowTree: false
           }));
-        } else if (query.hasOwnProperty('parentId')) {
-          this.disableInput = true;
-          this.searchQuery = 'Children of ' + query.parentId;
-          this.currentQuery = query.parentId;
-
-          // FIXME: This is temporary untill we have support for parent.id/number in search endpoint
-          const payload = space.links.self.split('spaces')[0] + 'workitems/' + query.parentId + '/children';
-          this.store.dispatch(new WorkItemActions.GetWorkItemChildrenForQuery(payload));
         }
         if (query.hasOwnProperty('prevq')) {
           this.breadcrumbs = JSON.parse(query.prevq);
@@ -158,33 +150,22 @@ export class PlannerQueryComponent implements OnInit, OnDestroy, AfterViewChecke
     let queryParams = cloneDeep(this.route.snapshot.queryParams);
     let previousQuery;
     if (queryParams.hasOwnProperty('prevq')) {
-      if (queryParams.hasOwnProperty('parentId')) {
+      if (queryParams.hasOwnProperty('q')) {
         previousQuery = {
           prevq: [
             ...JSON.parse(queryParams.prevq),
-            {
-              parentId: queryParams.parentId
-            }
-          ]
-        };
-      } else if (queryParams.hasOwnProperty('q')) {
-        previousQuery = {
-          prevq: [
-            ...JSON.parse(queryParams.prevq),
-            {
-              q: queryParams.q
-            }
+            {q: queryParams.q}
           ]
         };
       }
     } else {
       previousQuery = {prevq: [queryParams]};
     }
+
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
-        parentId: workItem.id
-        ,
+        q: 'parent.number : ' + workItem.number,
         prevq: JSON.stringify(previousQuery.prevq)
       }
     });
